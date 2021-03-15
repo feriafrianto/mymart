6c3c07629e292f020f1a2ff5a4aafb5af8eeb636
feriafrianto <feriafrianto7@gmail.com>
2021-03-15 22:09:25 +0700

    2 Praktikum

---
+++ backend/views/item-category/_form.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\ItemCategory */
+/* @var $form yii\widgets\ActiveForm */
+/* Feri Afrianto / 211019107*/
+?>
+
+<div class="item-category-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'parent_category')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
